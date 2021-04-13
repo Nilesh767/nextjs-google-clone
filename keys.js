b64d3cd24a4d906022d96034212017d9ff060f10
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-04-13 16:00:16 +0530

    Added Environment variables for keys

---
+++ keys.js
@@ -0,0 +1,5 @@
+//api key from: https://developers.google.com/custom-search/v1/introduction#identify_your_application_to_google_with_api_key
+export const API_KEY = process.env.API_KEY;
+
+//context key from: https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = process.env.CONTEXT_KEY;
