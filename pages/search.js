bfa0315b9b47728b38a45d9f30256d34b1cf09e1
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-04-13 14:06:41 +0530

    added search functionality

--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,37 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Search/Header/Header";
 
-const Search = () => {
+import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response";
+
+const Search = ({ results }) => {
+  const router = useRouter();
   return (
     <div>
       <Head>
-        <title>Search Result</title>
+        <title>{router.query.query} - Google Search</title>
       </Head>
       <Header />
-      Search
+      
     </div>
   );
 };
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummy = true;
+  const startIndex = context.query.start || "0";
+  const data = useDummy
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.query}&start=${startIndex}`
+      ).then((response) => response.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
