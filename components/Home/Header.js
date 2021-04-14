348a9aba7903274bc3782354541137cc526d23e6
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-04-14 12:54:48 +0530

    added href links to header

--- components/Home/Header.js
+++ components/Home/Header.js
@@ -6,8 +6,19 @@ const Header = () => {
   return (
     <header className="flex w-full p-5 justify-between text-sm text-gray-700">
       <div className="flex space-x-4 items-center">
-        <p className="link">About</p>
-        <p className="link">Github</p>
+        <p className="link">
+          <a href="https://github.com/nilesh767" target="_blank">
+            About
+          </a>
+        </p>
+        <p className="link">
+          <a
+            href="https://github.com/Nilesh767/nextjs-google-clone"
+            target="_blank"
+          >
+            Github
+          </a>
+        </p>
       </div>
       <div className="flex space-x-4 items-center">
         <p className="link">Gmail</p>
